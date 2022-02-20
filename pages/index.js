4619f1267ef5d4d6a28ae05103d2ff6d2a4f8f1d
forresttanaka <fytanaka@stanford.edu>
2022-02-20 07:42:58 -0800

    Add temporary home-page CGI

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,22 @@
 // components
-import { Logo } from "../components/logo"
+import { useEffect } from "react"
 
 const Home = () => {
+  useEffect(() => {
+    // For the home page, add a full-page background image. You cannot do this in _document.js
+    // because there the body tag only gets rendered on the server and therefore can't be
+    // customized per page.
+    const defaultBodyClassName = document.body.className
+    document.body.className +=
+      " bg-[url('/img/home-bg.jpg')] dark:bg-[url('/img/home-dark-bg.jpg')] bg-center bg-no-repeat bg-cover"
+
+    return () => {
+      document.body.className = defaultBodyClassName
+    }
+  })
+
   return (
-    <div className="flex h-screen w-full grow items-center justify-items-center">
-      <div className="w-full text-center text-xl uppercase tracking-[0.4rem]">
-        <Logo />
-        Impact of Genomic Variation on Function
-      </div>
-    </div>
+    <div className="flex h-screen w-full grow items-center justify-items-center" />
   )
 }
 
