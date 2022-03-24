a6a709babb7e088244d84440fe2dab48338d8a94
forresttanaka <ftanaka@mac.com>
2022-03-24 16:30:31 -0700

    IGVF-56-update-awards
    Fix crash when award doesn’t have a PI. Add some styling to award search results.

--- components/page-title.js
+++ components/page-title.js
@@ -2,7 +2,9 @@
  * Show a standard page title for the top of any page.
  */
 const PageTitle = ({ children }) => {
-  return <h1 className="text-4xl font-thin">{children}</h1>
+  return (
+    <h1 className="border-b border-gray-500 text-4xl font-thin">{children}</h1>
+  )
 }
 
 export default PageTitle
