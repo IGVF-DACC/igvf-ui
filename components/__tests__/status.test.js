c0b339f3ebe1499cd741f197eae21f7de5522e1a
forresttanaka <fytanaka@stanford.edu>
2022-05-04 15:57:19 -0700

    IGVF-101-status
    Get full coverage in status.js test.

--- components/__tests__/status.test.js
+++ components/__tests__/status.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react"
+import { render } from "@testing-library/react"
 import Status from "../status"
 
 describe("Test the status badges", () => {
@@ -7,8 +7,13 @@ describe("Test the status badges", () => {
     expect(container.getElementsByClassName("bg-[#00a99d]").length).toBe(1)
   })
 
-  it("generates the correct status-badge Tailwind CSS classes for the current state", () => {
+  it("generates the correct status-badge Tailwind CSS classes for the deleted state", () => {
     const { container } = render(<Status status="deleted" />)
     expect(container.getElementsByClassName("bg-[#ed1c24]").length).toBe(1)
+  })
+
+  it("generates the correct status-badge Tailwind CSS classes for an unsupported state", () => {
+    const { container } = render(<Status status="unsupported" />)
+    expect(container.getElementsByClassName("bg-[#d0d0d0]").length).toBe(1)
   })
 })
