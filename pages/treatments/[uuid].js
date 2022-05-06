d5bddeabdd2035770402b81c5d02e7758d32ec2c
forresttanaka <fytanaka@stanford.edu>
2022-05-06 13:26:12 -0700

    IGVF-108-treatment
    Add collection and individual pages for treatments.

---
+++ pages/treatments/[uuid].js
@@ -0,0 +1,114 @@
+// node_modules
+import Link from "next/link"
+import PropTypes from "prop-types"
+// components
+import Breadcrumbs from "../../components/breadcrumbs"
+import {
+  DataArea,
+  DataAreaTitle,
+  DataItem,
+  DataItemLabel,
+  DataItemValue,
+} from "../../components/data-area"
+import PagePreamble from "../../components/page-preamble"
+import Status from "../../components/status"
+// libs
+import buildBreadcrumbs from "../../libs/breadcrumbs"
+import Request from "../../libs/request"
+
+const Treatment = ({ treatment }) => {
+  return (
+    <>
+      <Breadcrumbs />
+      <PagePreamble />
+      <DataArea>
+        <DataItem>
+          <DataItemLabel>Status</DataItemLabel>
+          <DataItemValue>
+            <Status status={treatment.status} />
+          </DataItemValue>
+        </DataItem>
+        <DataItem>
+          <DataItemLabel>Treatment Term ID</DataItemLabel>
+          <DataItemValue>{treatment.treatment_term_id}</DataItemValue>
+        </DataItem>
+        <DataItem>
+          <DataItemLabel>Treatment Term Name</DataItemLabel>
+          <DataItemValue>{treatment.treatment_term_name}</DataItemValue>
+        </DataItem>
+        <DataItem>
+          <DataItemLabel>Treatment Type</DataItemLabel>
+          <DataItemValue>{treatment.treatment_type}</DataItemValue>
+        </DataItem>
+        <DataItem>
+          <DataItemLabel>Amount</DataItemLabel>
+          <DataItemValue>
+            {treatment.amount} {treatment.amount_units}
+          </DataItemValue>
+        </DataItem>
+        {treatment.duration && (
+          <DataItem>
+            <DataItemLabel>Duration</DataItemLabel>
+            <DataItemValue>
+              {treatment.duration} {treatment.duration_units}
+              {treatment.duration === 1 ? "" : "s"}
+            </DataItemValue>
+          </DataItem>
+        )}
+        {treatment.pH && (
+          <DataItem>
+            <DataItemLabel>pH</DataItemLabel>
+            <DataItemValue>{treatment.pH}</DataItemValue>
+          </DataItem>
+        )}
+        {treatment.purpose && (
+          <DataItem>
+            <DataItemLabel>Purpose</DataItemLabel>
+            <DataItemValue>{treatment.purpose}</DataItemValue>
+          </DataItem>
+        )}
+        {treatment.post_treatment_time && (
+          <DataItem>
+            <DataItemLabel>Post-Treatment Time</DataItemLabel>
+            <DataItemValue>
+              {treatment.post_treatment_time}{" "}
+              {treatment.post_treatment_time_units}
+              {treatment.post_treatment_time === 1 ? "" : "s"}
+            </DataItemValue>
+          </DataItem>
+        )}
+        {treatment["temperature (Celsius)"] && (
+          <DataItem>
+            <DataItemLabel>Temperature</DataItemLabel>
+            <DataItemValue>
+              {treatment["temperature (Celsius)"]} &deg;C
+            </DataItemValue>
+          </DataItem>
+        )}
+      </DataArea>
+    </>
+  )
+}
+
+Treatment.propTypes = {
+  // Technical treatment to display
+  treatment: PropTypes.object.isRequired,
+}
+
+export default Treatment
+
+export const getServerSideProps = async ({ params, req }) => {
+  const request = new Request(req?.headers?.cookie)
+  const treatment = await request.getObject(`/treatments/${params.uuid}/`)
+  if (treatment && treatment.status !== "error") {
+    const breadcrumbs = await buildBreadcrumbs(treatment, "treatment_term_id")
+    return {
+      props: {
+        treatment,
+        pageContext: { title: treatment.treatment_term_id },
+        breadcrumbs,
+      },
+    }
+  }
+  return { notFound: true }
+}
---
+++ pages/treatments/index.js
@@ -0,0 +1,69 @@
+// node_modules
+import PropTypes from "prop-types"
+// components
+import Breadcrumbs from "../../components/breadcrumbs"
+import {
+  Collection,
+  CollectionCount,
+  CollectionItem,
+  CollectionItemName,
+} from "../../components/collection"
+import NoCollectionData from "../../components/no-collection-data"
+import PagePreamble from "../../components/page-preamble"
+// libs
+import buildBreadcrumbs from "../../libs/breadcrumbs"
+import Request from "../../libs/request"
+
+const TreatmentList = ({ treatments }) => {
+  return (
+    <>
+      <Breadcrumbs />
+      <PagePreamble />
+      <Collection>
+        {treatments.length > 0 ? (
+          <>
+            <CollectionCount count={treatments.length} />
+            {treatments.map((treatment) => (
+              <CollectionItem
+                key={treatment.uuid}
+                href={treatment["@id"]}
+                label={`Treatment ${treatment.treatment_term_id}`}
+                status={treatment.status}
+              >
+                <CollectionItemName>
+                  {treatment.treatment_term_id}
+                </CollectionItemName>
+                {treatment.purpose && <div>{treatment.purpose}</div>}
+                {treatment.treatment_term_name && (
+                  <div>{treatment.treatment_term_name}</div>
+                )}
+              </CollectionItem>
+            ))}
+          </>
+        ) : (
+          <NoCollectionData />
+        )}
+      </Collection>
+    </>
+  )
+}
+
+TreatmentList.propTypes = {
+  // Technical samples to display in the list
+  treatments: PropTypes.arrayOf(PropTypes.object).isRequired,
+}
+
+export default TreatmentList
+
+export const getServerSideProps = async ({ req }) => {
+  const request = new Request(req?.headers?.cookie)
+  const treatments = await request.getCollection("treatments")
+  const breadcrumbs = await buildBreadcrumbs(treatments, "title")
+  return {
+    props: {
+      treatments: treatments["@graph"],
+      pageContext: { title: treatments.title },
+      breadcrumbs,
+    },
+  }
+}
