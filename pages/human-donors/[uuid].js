56f47874cc831471ee93399c88423033b039c75e
Forrest Tanaka <fytanaka@stanford.edu>
2022-05-09 16:08:06 -0700

    IGVF-116 Human and rodent donor pages (#33)
    * Initial display of human donor collections and individual pages.
    * Finish the individual page for human donors, and add its navigation item and icon.
    * Implement rodent donor collection and individual pages. Refactor the human donor individual page to share code with the roden donor page.
    * Add to the Cypress navigation test.
    * Update outdated npm packages.
    * Alter SortableGrid to use the now-optional keyProp.

---
+++ pages/human-donors/[uuid].js
@@ -0,0 +1,167 @@
+// node_modules
+import dayjs from "dayjs"
+import Link from "next/link"
+import PropTypes from "prop-types"
+// components
+import Breadcrumbs from "../../components/breadcrumbs"
+import {
+  DataArea,
+  DataAreaTitle,
+  DataItem,
+  DataItemLabel,
+  DataItemValue,
+} from "../../components/data-area"
+import { DataGridContainer } from "../../components/data-grid"
+import ExternalResources from "../../components/external-resources"
+import PagePreamble from "../../components/page-preamble"
+import SeparatedList from "../../components/separated-list"
+import SortableGrid from "../../components/sortable-grid"
+import Status from "../../components/status"
+// libs
+import buildBreadcrumbs from "../../libs/breadcrumbs"
+import { formatDateRange } from "../../libs/dates"
+import Request from "../../libs/request"
+
+const healthStatusHistoryColumns = [
+  {
+    id: "dates",
+    title: "Health Change Date",
+    display: ({ source }) =>
+      formatDateRange(source.date_start, source.date_end),
+    sorter: (healthStatus) =>
+      dayjs(healthStatus.date_start || healthStatus.date_end).unix(),
+  },
+  {
+    id: "health_description",
+    title: "Description",
+    isSortable: false,
+  },
+]
+
+const HumanDonor = ({ donor, award, lab, parents }) => {
+  return (
+    <>
+      <Breadcrumbs />
+      <PagePreamble />
+      <DataArea>
+        <DataItem>
+          <DataItemLabel>Status</DataItemLabel>
+          <DataItemValue>
+            <Status status={donor.status} />
+          </DataItemValue>
+        </DataItem>
+        <DataItem>
+          <DataItemLabel>Taxa Identifier</DataItemLabel>
+          <DataItemValue>{donor.taxon_id}</DataItemValue>
+        </DataItem>
+        {donor.sex && (
+          <DataItem>
+            <DataItemLabel>Sex</DataItemLabel>
+            <DataItemValue>{donor.sex}</DataItemValue>
+          </DataItem>
+        )}
+        {parents.length > 0 && (
+          <DataItem>
+            <DataItemLabel>Parents</DataItemLabel>
+            <SeparatedList>
+              {parents.map((parent) => (
+                <Link href={parent["@id"]} key={parent.uuid}>
+                  <a aria-label={`Parent Donor ${parent.accession}`}>
+                    {parent.accession}
+                  </a>
+                </Link>
+              ))}
+            </SeparatedList>
+          </DataItem>
+        )}
+        {donor.ethnicity && (
+          <DataItem>
+            <DataItemLabel>Ethnicity</DataItemLabel>
+            <DataItemValue>{donor.ethnicity.join(", ")}</DataItemValue>
+          </DataItem>
+        )}
+      </DataArea>
+      <ExternalResources resources={donor.external_resources} />
+      {donor.health_status_history?.length > 0 && (
+        <>
+          <DataAreaTitle>Health Status History</DataAreaTitle>
+          <DataGridContainer>
+            <SortableGrid
+              data={donor.health_status_history}
+              columns={healthStatusHistoryColumns}
+            />
+          </DataGridContainer>
+        </>
+      )}
+      <DataAreaTitle>Attribution</DataAreaTitle>
+      <DataArea>
+        <DataItem>
+          <DataItemLabel>Award</DataItemLabel>
+          <DataItemValue>
+            <Link href={award["@id"]}>
+              <a>{award.name}</a>
+            </Link>
+          </DataItemValue>
+        </DataItem>
+        <DataItem>
+          <DataItemLabel>Lab</DataItemLabel>
+          <DataItemValue>
+            <Link href={lab["@id"]}>
+              <a>{lab.title}</a>
+            </Link>
+          </DataItemValue>
+        </DataItem>
+        {donor.url && (
+          <DataItem>
+            <DataItemValue>
+              <a href={donor.url} target="_blank" rel="noreferrer">
+                Additional Information
+              </a>
+            </DataItemValue>
+          </DataItem>
+        )}
+      </DataArea>
+    </>
+  )
+}
+
+HumanDonor.propTypes = {
+  // Technical sample to display
+  donor: PropTypes.object.isRequired,
+  // Award applied to this technical sample
+  award: PropTypes.shape({
+    "@id": PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+  }).isRequired,
+  // Lab that submitted this technical sample
+  lab: PropTypes.shape({
+    "@id": PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+  }).isRequired,
+  // Parents of this donor
+  parents: PropTypes.arrayOf(PropTypes.object).isRequired,
+}
+
+export default HumanDonor
+
+export const getServerSideProps = async ({ params, req }) => {
+  const request = new Request(req?.headers?.cookie)
+  const donor = await request.getObject(`/human-donors/${params.uuid}/`)
+  if (donor && donor.status !== "error") {
+    const award = await request.getObject(donor.award)
+    const lab = await request.getObject(donor.lab)
+    const parents = await request.getMultipleObjects(donor.parents)
+    const breadcrumbs = await buildBreadcrumbs(donor, "accession")
+    return {
+      props: {
+        donor,
+        award,
+        lab,
+        parents,
+        pageContext: { title: donor.accession },
+        breadcrumbs,
+      },
+    }
+  }
+  return { notFound: true }
+}
