3d2db69552b1e8b61b765ead3e8cc7a166e36b16
forresttanaka <fytanaka@stanford.edu>
2022-06-06 08:35:45 -0700

    IGVF-93-cypress-auth0

--- docker/nextjs/entrypoint.sh
+++ docker/nextjs/entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
-echo 'ENV ENV ENV'
-cat /igvf-ui/.env.local
+
+echo "Check that we have NEXT_PUBLIC_AUTH0_ISSUER_BASE_DOMAIN vars"
+test -n "$NEXT_PUBLIC_AUTH0_ISSUER_BASE_DOMAIN"
+
+find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#APP_NEXT_PUBLIC_AUTH0_ISSUER_BASE_DOMAIN#$NEXT_PUBLIC_AUTH0_ISSUER_BASE_DOMAIN#g"
+
 
 exec "$@"
