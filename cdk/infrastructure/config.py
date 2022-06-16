0c2a5bbf5b5ad95772208c410a2b112e7db1c1f8
Keenan Graham <ksgraham@stanford.edu>
2022-06-16 11:08:16 -0700

    Add tests

--- cdk/infrastructure/config.py
+++ cdk/infrastructure/config.py
@@ -1,6 +1,54 @@
-config = {
-    'org_name': 'igvf-dacc',
-    'project_name': 'igvf-ui',
-    'default_branch': 'main',
-    'region': 'us-west-2',
+from dataclasses import dataclass
+
+from typing import Any
+from typing import Dict
+from typing import Optional
+
+
+config: Dict[str, Any] = {
+    'environment': {
+        'demo': {
+            'pipeline': 'DemoDeploymentPipelineStack',
+        },
+        'dev': {
+            'pipeline': 'DemoDeploymentPipelineStack',
+        },
+        'main': {
+            'staging': {},
+            'test': {},
+            'prod': {},
+        }
+    }
 }
+
+
+@dataclass
+class Common:
+    organization_name: str = 'igvf-dacc'
+    project_name: str = 'igvf-ui'
+    default_region: str = 'us-west-2'
+    aws_cdk_version: str = '2.21.0'
+
+
+@dataclass
+class Config:
+    name: str
+    branch: str
+    pipeline: str
+    common: Common = Common()
+
+
+def build_config_from_name(name: str, **kwargs: Any) -> Config:
+    return Config(
+        **{
+            **config['environment'][name],
+            **kwargs,
+            **{'name': name},
+        }
+    )
+
+
+def get_config_name_from_branch(branch: str) -> str:
+    if branch == 'dev':
+        return 'dev'
+    return 'demo'
