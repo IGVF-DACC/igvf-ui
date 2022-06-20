c1c19afb0a857567ba6595b2c5c0229634120af7
Keenan Graham <ksgraham@stanford.edu>
2022-06-20 12:45:50 -0700

    Add tests

---
+++ cdk/tests/unit/test_stacks_frontend.py
@@ -0,0 +1,77 @@
+import pytest
+
+from aws_cdk.assertions import Template
+
+
+def test_stacks_frontend_initialize_frontend_stack(config):
+    from aws_cdk import App
+    from infrastructure.stacks.frontend import FrontendStack
+    from infrastructure.constructs.existing import igvf_dev
+    app = App()
+    frontend_stack = FrontendStack(
+        app,
+        'TestFrontendStack',
+        config=config,
+        existing_resources_class=igvf_dev.Resources,
+        env=igvf_dev.US_WEST_2,
+    )
+    template = Template.from_stack(frontend_stack)
+    template.has_resource_properties(
+        'AWS::ECS::Service',
+        {
+            'Cluster': {
+                'Ref': 'EcsDefaultClusterMnL3mNNYNDemoVpc278C9613'
+            },
+            'DeploymentConfiguration': {
+                'DeploymentCircuitBreaker': {
+                    'Enable': True,
+                    'Rollback': True
+                },
+                'MaximumPercent': 200,
+                'MinimumHealthyPercent': 50
+            },
+            'DeploymentController': {
+                'Type': 'ECS'
+            },
+            'DesiredCount': 1,
+            'EnableECSManagedTags': False,
+            'EnableExecuteCommand': True,
+            'HealthCheckGracePeriodSeconds': 60,
+            'LaunchType': 'FARGATE',
+            'LoadBalancers': [
+                {
+                    'ContainerName': 'nextjs',
+                    'ContainerPort': 3000,
+                    'TargetGroupArn': {
+                        'Ref': 'FrontendFargateLBPublicListenerECSGroupB493F3AB'
+                    }
+                }
+            ],
+            'NetworkConfiguration': {
+                'AwsvpcConfiguration': {
+                    'AssignPublicIp': 'ENABLED',
+                    'SecurityGroups': [
+                        {
+                            'Fn::GetAtt': [
+                                'FrontendFargateServiceSecurityGroup52B6B765',
+                                'GroupId'
+                            ]
+                        }
+                    ],
+                    'Subnets': [
+                        's-12345',
+                        's-67890'
+                    ]
+                }
+            },
+            'Tags': [
+                {
+                    'Key': 'branch',
+                    'Value': 'some-branch'
+                }
+            ],
+            'TaskDefinition': {
+                'Ref': 'FrontendFargateTaskDefC3798D1C'
+            }
+        }
+    )
---
+++ cdk/tests/unit/test_stacks_pipeline.py
@@ -0,0 +1,42 @@
+import pytest
+
+import aws_cdk as core
+import aws_cdk.assertions as assertions
+
+from infrastructure.stacks.pipeline import DemoDeploymentPipelineStack
+
+
+def test_stacks_pipeline_demo_deployment_pipeline_stack_initialized(config):
+    from infrastructure.constructs.existing import igvf_dev
+    app = core.App()
+    stack = DemoDeploymentPipelineStack(
+        app,
+        'CDStack',
+        existing_resources_class=igvf_dev.Resources,
+        config=config,
+        env=igvf_dev.US_WEST_2,
+    )
+    template = assertions.Template.from_stack(stack)
+    template.resource_count_is(
+        'AWS::CodePipeline::Pipeline',
+        1
+    )
+    code_pipeline_resource = template.find_resources(
+        'AWS::CodePipeline::Pipeline'
+    )
+    stages = code_pipeline_resource.get(
+        'DemoDeploymentPipelineCodePipeline8A07F2A4'
+    ).get(
+        'Properties'
+    ).get(
+        'Stages'
+    )
+    expected = [
+        'Source',
+        'Build',
+        'UpdatePipeline',
+        'Assets',
+        'igvf-ui-some-branch-DeployDevelopment'
+    ]
+    actual = [stage['Name'] for stage in stages]
+    assert len(stages) == 5
