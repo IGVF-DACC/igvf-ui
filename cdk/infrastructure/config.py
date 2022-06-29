924b9e4db4dddf212c9bcb55fab81108026fcf48
Keenan Graham <ksgraham@stanford.edu>
2022-06-29 10:29:17 -0700

    Remove demo url

--- cdk/infrastructure/config.py
+++ cdk/infrastructure/config.py
@@ -11,7 +11,6 @@ config: Dict[str, Any] = {
     'environment': {
         'demo': {
             'pipeline': 'DemoDeploymentPipelineStack',
-            'backend_url': 'https://igvfd-igvf-206-share-cookies-across-subdomains.demo.igvf.org'
         },
         'dev': {
             'pipeline': 'DemoDeploymentPipelineStack',
