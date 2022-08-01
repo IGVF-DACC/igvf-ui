8cd3d84b7e2b6dd368b2b9a46bc83edf9f99fdf3
forresttanaka <fytanaka@stanford.edu>
2022-08-01 14:10:58 -0700

    IGVF-252-genes
    Temporary demo data provider setting.

--- cdk/infrastructure/config.py
+++ cdk/infrastructure/config.py
@@ -11,6 +11,7 @@ config: Dict[str, Any] = {
     'environment': {
         'demo': {
             'pipeline': 'DemoDeploymentPipelineStack',
+            'backend_url': 'https://igvfd-test-data-inserts.demo.igvf.org'
         },
         'dev': {
             'pipeline': 'DemoDeploymentPipelineStack',
