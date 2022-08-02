2cf74268b90b004f5962169e2a21ece97f113462
forresttanaka <fytanaka@stanford.edu>
2022-08-02 16:37:48 -0700

    IGVF-237-cypress-10
    Add e2e testing configuration to the cypress docker config.

--- docker/cypress/cypress-docker.config.js
+++ docker/cypress/cypress-docker.config.js
@@ -3,4 +3,10 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   defaultCommandTimeout: 30000,
   watchForFileChanges: true,
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
 });
