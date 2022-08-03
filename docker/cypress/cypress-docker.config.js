7e7a89d8709e1df68a6c3d7070f68b6f4318d40b
Forrest Tanaka <fytanaka@stanford.edu>
2022-08-03 15:33:02 -0700

    IGVF-237 Update Cypress to version 10 (#58)
    Commit Logs
    
    * Update the non-cypress npm packages.
    * Update the Cypress configuration for Cypress 10.
    * Change Docker config for Cypress.
    * Update Cypress version in main docker config.
    * Update Docker Cypress configuration, as well as Docker config copy commands. Will keep the old cypress-docker.json for now, but I need to delete it later.
    * Add e2e testing configuration to the cypress docker config.
    * Update Cypress Dockerfile to use 10.4.0.
    * Try to get Docker Cypress working. It now works locally. See if it works in CircleCI.
    * Remove now-unused old Cypress config file. Remove copying of cypress.env.json to the test volume, as this file doesn’t exist in CircleCI.
    * Try Keenan’s suggestion of removing a couple now-unneeded files.

---
+++ docker/cypress/cypress-docker.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  defaultCommandTimeout: 30000,
+  watchForFileChanges: true,
+
+  e2e: {
+    baseUrl: "http://localhost:3000",
+    setupNodeEvents() {
+      // implement node event listeners here
+    },
+  },
+});
