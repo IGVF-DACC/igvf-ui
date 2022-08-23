ed98b952d016d11a8967fa70dac6dcc05a9592b1
forresttanaka <fytanaka@stanford.edu>
2022-08-22 17:20:25 -0700

    IGVF-284-life-stage
    Changed “Biosample Collection Age” back to “Age” and added an explanatory note after the age display, regardless of the contents of the age string.

--- components/common-data-items.js
+++ components/common-data-items.js
@@ -17,6 +17,7 @@ import SeparatedList from "./separated-list";
 import SourceProp from "./source-prop";
 // lib
 import { formatDate } from "../lib/dates";
+import Instruction from "./instruction";
 
 /**
  * Display the data items common to all donor-derived objects.
@@ -184,7 +185,7 @@ export const BiosampleDataItems = ({
       )}
       {biosample.age && (
         <>
-          <DataItemLabel>Biosample Collection Age</DataItemLabel>
+          <DataItemLabel>Age</DataItemLabel>
           <DataItemValue>
             {biosample.age}
             {biosample.age_units ? (
@@ -196,6 +197,7 @@ export const BiosampleDataItems = ({
             ) : (
               ""
             )}
+            <Instruction.Note>at time of sample collection</Instruction.Note>
           </DataItemValue>
         </>
       )}
