e522297080b92bdfd84fe4bfe7faa692223000e5
Keenan Graham <ksgraham@stanford.edu>
2022-12-13 19:15:12 -0800

    Add pipeline stack test

--- cdk/tests/unit/test_stacks_pipeline.py
+++ cdk/tests/unit/test_stacks_pipeline.py
@@ -80,3 +80,43 @@ def test_stacks_pipeline_dev_deployment_pipeline_stack_initialized(pipeline_conf
     for e in expected:
         assert e in actual
     assert len(stages) == 5
+
+
+def test_stacks_pipeline_production_deployment_pipeline_stack_initialized(production_pipeline_config):
+    from infrastructure.stacks.pipeline import ProductionDeploymentPipelineStack
+    from infrastructure.constructs.existing import igvf_dev
+    app = core.App()
+    stack = ProductionDeploymentPipelineStack(
+        app,
+        'ProductionDeploymentPipelineStack',
+        existing_resources_class=igvf_dev.Resources,
+        config=production_pipeline_config,
+        env=igvf_dev.US_WEST_2,
+    )
+    template = assertions.Template.from_stack(stack)
+    template.resource_count_is(
+        'AWS::CodePipeline::Pipeline',
+        1
+    )
+    code_pipeline_resource = template.find_resources(
+        'AWS::CodePipeline::Pipeline'
+    )
+    stages = code_pipeline_resource.get(
+        'ProductionDeploymentPipelineE21CAFC5',
+    ).get(
+        'Properties'
+    ).get(
+        'Stages'
+    )
+    expected = [
+        'Source',
+        'Build',
+        'UpdatePipeline',
+        'Assets',
+        'igvf-ui-some-branch-StagingDeployStage',
+        'ProductionAndSandboxDeployWave',
+    ]
+    actual = [stage['Name'] for stage in stages]
+    for e in expected:
+        assert e in actual
+    assert len(stages) == 6
