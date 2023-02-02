f147c74e750750c76ccd2c7659706492f88f4999
Mingjie Li <mingjiecn@gmail.com>
2023-02-02 13:48:34 -0600

    add file set list and analysis set object page

---
+++ components/search/list-renderer/analysis-set.js
@@ -0,0 +1,49 @@
+// node_modules
+import PropTypes from "prop-types";
+// components
+import {
+  SearchListItemContent,
+  SearchListItemMeta,
+  SearchListItemMain,
+  SearchListItemStatus,
+  SearchListItemTitle,
+  SearchListItemType,
+  SearchListItemUniqueId,
+} from "./search-list-item";
+
+const AnalysisSet = ({ item: analysisSet, accessoryData }) => {
+//   const ethnicities =
+//     analysisSet.ethnicity?.length > 0 ? analysisSet.ethnicity.join(", ") : "";
+//   const sex = analysisSet.sex || "";
+//   const title = [ethnicities, sex].filter(Boolean);
+//   const lab = accessoryData?.[analysisSet.lab];
+
+  return (
+    <SearchListItemContent>
+      <SearchListItemMain>
+        <SearchListItemUniqueId>
+          <SearchListItemType item={analysisSet} />
+          {analysisSet.uuid}
+        </SearchListItemUniqueId>
+        <SearchListItemTitle>
+        <SearchListItemType item={analysisSet} />
+        </SearchListItemTitle>
+
+      </SearchListItemMain>
+      <SearchListItemStatus item={analysisSet} />
+    </SearchListItemContent>
+  );
+};
+
+AnalysisSet.propTypes = {
+  // Single human-donor search-result object to display on a search-result list page
+  item: PropTypes.object.isRequired,
+  // Accessory data to display for all search-result objects
+  accessoryData: PropTypes.object,
+};
+
+// AnalysisSet.getAccessoryDataPaths = (humanDonors) => {
+//   return humanDonors.map((humanDonor) => humanDonor.lab).filter(Boolean);
+// };
+
+export default AnalysisSet;
---
+++ pages/analysis-sets/[uuid].js
@@ -0,0 +1,143 @@
+// node_modules
+import dayjs from "dayjs";
+import PropTypes from "prop-types";
+// components
+import Attribution from "../../components/attribution";
+import Breadcrumbs from "../../components/breadcrumbs";
+import { DonorDataItems } from "../../components/common-data-items";
+import {
+  DataArea,
+  DataAreaTitle,
+  DataItemLabel,
+  DataItemValue,
+  DataPanel,
+} from "../../components/data-area";
+import { DataGridContainer } from "../../components/data-grid";
+import DocumentTable from "../../components/document-table";
+import ExternalResources from "../../components/external-resources";
+import PagePreamble from "../../components/page-preamble";
+import SortableGrid from "../../components/sortable-grid";
+import Status from "../../components/status";
+import { EditableItem } from "../../components/edit";
+// lib
+import buildBreadcrumbs from "../../lib/breadcrumbs";
+import { formatDateRange } from "../../lib/dates";
+import errorObjectToProps from "../../lib/errors";
+import FetchRequest from "../../lib/fetch-request";
+import AliasList from "../../components/alias-list";
+
+/**
+ * Defines the columns for the health-status table.
+ */
+const healthStatusHistoryColumns = [
+  {
+    id: "dates",
+    title: "Health Change Date",
+    display: ({ source }) =>
+      formatDateRange(source.date_start, source.date_end),
+    sorter: (healthStatus) =>
+      dayjs(healthStatus.date_start || healthStatus.date_end).unix(),
+  },
+  {
+    id: "health_description",
+    title: "Description",
+    isSortable: false,
+  },
+];
+
+const AnalysisSet = ({
+  analysisSet,
+  award = null,
+  documents,
+  donors,
+  lab = null,
+  samples,
+  
+}) => {
+  return (
+    <>
+      <Breadcrumbs />
+      <EditableItem item={analysisSet}>
+        <PagePreamble />
+        <DataPanel>
+          <DataArea>
+          <DataItemLabel>Aliases</DataItemLabel>
+          <DataItemValue>
+            <AliasList aliases={analysisSet.aliases} />
+          </DataItemValue>
+            <DataItemLabel>Status</DataItemLabel>
+            <DataItemValue>
+              <Status status={analysisSet.status} />
+            </DataItemValue>
+          </DataArea>
+        </DataPanel>
+
+        {documents.length > 0 && (
+          <>
+            <DataAreaTitle>Documents</DataAreaTitle>
+            <DocumentTable documents={documents} />
+          </>
+        )}
+
+        <Attribution award={award} lab={lab} />
+      </EditableItem>
+    </>
+  );
+};
+
+AnalysisSet.propTypes = {
+  // Technical sample to display
+  donor: PropTypes.object.isRequired,
+  // Award applied to this technical sample
+  award: PropTypes.object,
+  // Documents associated with the cell-line
+  documents: PropTypes.arrayOf(PropTypes.object).isRequired,
+  // Lab that submitted this technical sample
+  lab: PropTypes.object,
+  // Parents of this donor
+  parents: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default AnalysisSet;
+
+export const getServerSideProps = async ({ params, req }) => {
+  const request = new FetchRequest({ cookie: req.headers.cookie });
+  const analysisSet = await request.getObject(`/analysis-sets/${params.uuid}/`);
+  if (FetchRequest.isResponseSuccess(analysisSet)) {
+    const award = await request.getObject(analysisSet.award, null);
+    const documents = analysisSet.documents
+      ? await request.getMultipleObjects(analysisSet.documents, null, {
+          filterErrors: true,
+        })
+      : [];
+    const lab = await request.getObject(analysisSet.lab, null);
+    const samples = analysisSet.samples
+      ? await request.getMultipleObjects(analysisSet.samples, null, {
+          filterErrors: true,
+        })
+      : [];
+    const donors = analysisSet.donors
+      ? await request.getMultipleObjects(analysisSet.donors, null, {
+          filterErrors: true,
+        })
+      : [];
+    const breadcrumbs = await buildBreadcrumbs(
+      analysisSet,
+      "accession",
+      req.headers.cookie
+    );
+    return {
+      props: {
+        analysisSet,
+        award,
+        documents,
+        donors,
+        lab,
+        samples,    
+        pageContext: { title: analysisSet.accession },
+        breadcrumbs,
+      },
+    };
+  }
+  return errorObjectToProps(analysisSet);
+};
