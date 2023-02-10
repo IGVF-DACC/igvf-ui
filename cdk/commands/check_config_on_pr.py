90bd19089861f406bd81f37c912a0eb113ec0375
Otto Jolanki <ojolanki@stanford.edu>
2023-02-10 14:42:44 -0800

    run pr check separate from other tests

---
+++ cdk/commands/check_config_on_pr.py
@@ -0,0 +1,10 @@
+def check_demo_config_on_pr():
+    from infrastructure.config import config
+    assert 'backend_url' not in config['environment']['demo']
+    assert config['environment']['demo']['tags'] == [
+        ('time-to-live-hours', '72'),
+        ('turn-off-on-friday-night', 'yes')]
+
+
+if __name__ == '__main__':
+    check_demo_config_on_pr()
