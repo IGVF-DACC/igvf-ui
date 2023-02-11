e0e5d0842508446d7710336c421c26cf0d0d8dea
Otto Jolanki <ojolanki@stanford.edu>
2023-02-10 16:09:55 -0800

    fix test

--- cdk/commands/check_config_on_pr.py
+++ cdk/commands/check_config_on_pr.py
@@ -2,7 +2,7 @@ def check_demo_config_on_pr():
     from infrastructure.config import config
     assert 'backend_url' not in config['environment']['demo']
     assert config['environment']['demo']['tags'] == [
-        ('time-to-live-hours', '71'),
+        ('time-to-live-hours', '72'),
         ('turn-off-on-friday-night', 'yes')]
 
 
