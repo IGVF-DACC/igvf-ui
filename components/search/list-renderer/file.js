63425ce9d8dd26ff2c98e796665eccadc8b066b0
Mingjie Li <mingjiecn@gmail.com>
2023-02-16 17:58:12 -0600

    add file in list-renderer folder

---
+++ components/search/list-renderer/file.js
@@ -0,0 +1,47 @@
+// node_modules
+import PropTypes from "prop-types";
+// components
+import {
+  SearchListItemContent,
+  SearchListItemMeta,
+  SearchListItemMain,
+  SearchListItemStatus,
+  SearchListItemTitle,
+  SearchListItemType,
+  SearchListItemUniqueId,
+} from "./search-list-item";
+
+export default function File({ item: file, accessoryData }) {
+  const titleElements = [file.file_format, file.content_type];
+  const lab = accessoryData?.[file.lab];
+
+  return (
+    <SearchListItemContent>
+      <SearchListItemMain>
+        <SearchListItemUniqueId>
+          <SearchListItemType item={file} />
+          {file.accession}
+        </SearchListItemUniqueId>
+        <SearchListItemTitle>{titleElements.join(" - ")}</SearchListItemTitle>
+        {lab && (
+          <SearchListItemMeta>
+            <div key="lab">{lab.title}</div>
+          </SearchListItemMeta>
+        )}
+      </SearchListItemMain>
+      <SearchListItemStatus item={file} />
+    </SearchListItemContent>
+  );
+}
+
+File.propTypes = {
+  // Single biosample-derived search-result object to display on a search-result list page
+  item: PropTypes.object.isRequired,
+  // Accessory data to display for all search-result objects
+  accessoryData: PropTypes.object,
+};
+
+File.getAccessoryDataPaths = (files) => {
+  const labs = files.map((file) => file.lab);
+  return labs;
+};
