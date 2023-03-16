e7c9a98de18e14bc162d1309f626e994d66477e1
Eric Douglass <ericdlrv@stanford.edu>
2023-03-16 11:28:02 -0700

    Collections property is now displayed in the ATtribution component and added in human donor

--- pages/human-donors/[uuid].js
+++ pages/human-donors/[uuid].js
@@ -1,5 +1,4 @@
 // node_modules
-import dayjs from "dayjs";
 import PropTypes from "prop-types";
 // components
 import Attribution from "../../components/attribution";
@@ -12,38 +11,16 @@ import {
   DataItemValue,
   DataPanel,
 } from "../../components/data-area";
-import { DataGridContainer } from "../../components/data-grid";
 import DocumentTable from "../../components/document-table";
 import ExternalResources from "../../components/external-resources";
 import PagePreamble from "../../components/page-preamble";
-import SortableGrid from "../../components/sortable-grid";
 import Status from "../../components/status";
 import { EditableItem } from "../../components/edit";
 // lib
 import buildBreadcrumbs from "../../lib/breadcrumbs";
-import { formatDateRange } from "../../lib/dates";
 import errorObjectToProps from "../../lib/errors";
 import FetchRequest from "../../lib/fetch-request";
 import PhenotypicFeatureTable from "../../components/phenotypic-feature-table";
-
-/**
- * Defines the columns for the health-status table.
- */
-const healthStatusHistoryColumns = [
-  {
-    id: "dates",
-    title: "Health Change Date",
-    display: ({ source }) =>
-      formatDateRange(source.date_start, source.date_end),
-    sorter: (healthStatus) =>
-      dayjs(healthStatus.date_start || healthStatus.date_end).unix(),
-  },
-  {
-    id: "health_description",
-    title: "Description",
-    isSortable: false,
-  },
-];
 
 export default function HumanDonor({
   donor,
@@ -76,32 +53,13 @@ export default function HumanDonor({
           </>
         )}
         <ExternalResources resources={donor.external_resources} />
-        {donor.health_status_history?.length > 0 && (
-          <>
-            <DataAreaTitle>Health Status History</DataAreaTitle>
-            <DataGridContainer>
-              <SortableGrid
-                data={donor.health_status_history}
-                columns={healthStatusHistoryColumns}
-              />
-            </DataGridContainer>
-          </>
-        )}
         {documents.length > 0 && (
           <>
             <DataAreaTitle>Documents</DataAreaTitle>
             <DocumentTable documents={documents} />
           </>
         )}
-        <Attribution award={award} lab={lab} />
-        {donor.collections?.length > 0 && (
-          <DataPanel>
-            <DataArea>
-              <DataItemLabel>Collections</DataItemLabel>
-              <DataItemValue>{donor.collections.join(", ")}</DataItemValue>
-            </DataArea>
-          </DataPanel>
-        )}
+        <Attribution award={award} lab={lab} collections={donor.collections} />
       </EditableItem>
     </>
   );
