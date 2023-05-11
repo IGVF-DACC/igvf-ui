beb863da66ba71b829ed95feeb371bc2d95ab8d3
Mingjie Li <44071821+mingjiecn@users.noreply.github.com>
2023-05-11 15:17:28 -0500

    IGVF-125 Add page to display graph.svg  (#166)
    * link to https://api.data.igvf.org/profiles/graph.svg
    * enable dark mode
    * use igvfd-dev demo
    * remove backend url

---
+++ pages/profiles/graph.svg.js
@@ -0,0 +1,39 @@
+// node_modules
+import FetchRequest from "../../lib/fetch-request";
+import PropTypes from "prop-types";
+
+// components
+import Breadcrumbs from "../../components/breadcrumbs";
+import { DataPanel, DataAreaTitle } from "../../components/data-area";
+import PagePreamble from "../../components/page-preamble";
+
+export default function GraphSvg({ graph }) {
+  return (
+    <>
+      <Breadcrumbs />
+      <PagePreamble />
+      <DataAreaTitle>Graph</DataAreaTitle>
+      <DataPanel>
+        <div
+          id="graph"
+          className="dark:[&>svg>g>polygon]:fill-transparent [&>svg]:h-full [&>svg]:w-auto overflow-y-auto"
+          dangerouslySetInnerHTML={{ __html: graph }}
+        />
+      </DataPanel>
+    </>
+  );
+}
+
+GraphSvg.propTypes = {
+  graph: PropTypes.string.isRequired,
+};
+
+export async function getServerSideProps({ req }) {
+  const request = new FetchRequest({ cookie: req.headers.cookie });
+  const graph = await request.getText("/profiles/graph.svg");
+  return {
+    props: {
+      graph,
+    },
+  };
+}
