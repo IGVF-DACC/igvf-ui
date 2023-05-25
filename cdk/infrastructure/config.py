249ccef6b78abb2fd61872e559bf5b8354db4198
forresttanaka <fytanaka@stanford.edu>
2023-05-25 15:13:01 -0700

    IGVF-374-search-output
    Now point back to the IGVF-650 igvfd demo for Meenakshi to test.

--- cdk/infrastructure/config.py
+++ cdk/infrastructure/config.py
@@ -52,7 +52,7 @@ config: Dict[str, Any] = {
                 'desired_count': 1,
                 'max_capacity': 4,
             },
-            'backend_url': 'https://igvfd-dev.demo.igvf.org',
+            'backend_url': 'https://igvfd-igvf-650-add-ids-fuzzysearch.demo.igvf.org',
             'tags': [
                 ('time-to-live-hours', '72'),
                 ('turn-off-on-friday-night', 'yes'),
