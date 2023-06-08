0a130feead998442efb520d4b7e6879607a29352
Forrest Tanaka <fytanaka@stanford.edu>
2023-06-08 15:08:51 -0700

    IGVF-731-special-search
    Revert temporary change to demo deploy config.

--- cdk/infrastructure/config.py
+++ cdk/infrastructure/config.py
@@ -52,9 +52,8 @@ config: Dict[str, Any] = {
                 'desired_count': 1,
                 'max_capacity': 4,
             },
-            'backend_url': 'https://igvfd-dev.demo.igvf.org',
             'tags': [
-                ('time-to-live-hours', '120'),
+                ('time-to-live-hours', '72'),
                 ('turn-off-on-friday-night', 'yes'),
             ],
         },
