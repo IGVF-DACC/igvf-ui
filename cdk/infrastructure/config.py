43ac01da6a474e79ecb0585affbf3c73a1d27b0d
Keenan Graham <ksgraham@stanford.edu>
2023-07-27 18:12:34 -0700

    Bump CDK version and update tests

--- cdk/infrastructure/config.py
+++ cdk/infrastructure/config.py
@@ -114,7 +114,7 @@ class Common:
     organization_name: str = 'igvf-dacc'
     project_name: str = 'igvf-ui'
     default_region: str = 'us-west-2'
-    aws_cdk_version: str = '2.61.0'
+    aws_cdk_version: str = '2.88.0'
 
 
 @dataclass
