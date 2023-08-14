8da59514949e83e97d6b9175a5ed444eabc6c80c
khine <khzlin@stanford.edu>
2023-08-14 00:19:48 -0700

    add modification-table

---
+++ components/modification-table.js
@@ -0,0 +1,48 @@
+// node_modules
+import PropTypes from "prop-types";
+// components
+import { DataGridContainer } from "./data-grid";
+import SortableGrid from "./sortable-grid";
+
+const modificationsColumns = [
+  {
+    id: "modality",
+    title: "Modality",
+  },
+  {
+    id: "cas",
+    title: "Cas",
+  },
+  {
+    id: "Cas Species",
+    title: "Cas Species",
+  },
+  {
+    id: "fused_domain",
+    title: "Fused Domain",
+  },
+  {
+    id: "tagged_protein",
+    title: "Tagged Protein",
+  },
+];
+
+/**
+ * Display a sortable table of the given treatments.
+ */
+export default function ModificationsTable({ modifications }) {
+  return (
+    <DataGridContainer>
+      <SortableGrid
+        data={modifications}
+        columns={modificationsColumns}
+        keyProp="@id"
+      />
+    </DataGridContainer>
+  );
+}
+
+ModificationsTable.propTypes = {
+  // Modifications to display
+  modifications: PropTypes.arrayOf(PropTypes.object),
+};
