b74034c3fe3e2f603ae30c56f8919bc831d425cf
KAndreeva <andreva@stanford.edu>
2023-08-29 19:26:55 -0400

    IGVF-836 Support display of cas_species, description, source and documents on Modification page.

--- pages/modifications/[uuid].js
+++ pages/modifications/[uuid].js
@@ -6,17 +6,21 @@ import Attribution from "../../components/attribution";
 import Breadcrumbs from "../../components/breadcrumbs";
 import {
   DataArea,
+  DataAreaTitle,
   DataItemLabel,
   DataItemValue,
   DataPanel,
 } from "../../components/data-area";
+import DocumentTable from "../../components/document-table";
 import { EditableItem } from "../../components/edit";
+import ProductInfo from "../../components/product-info";
 import JsonDisplay from "../../components/json-display";
 import ObjectPageHeader from "../../components/object-page-header";
 import PagePreamble from "../../components/page-preamble";
 // lib
 import buildAttribution from "../../lib/attribution";
 import buildBreadcrumbs from "../../lib/breadcrumbs";
+import { requestDocuments } from "../../lib/common-requests";
 import errorObjectToProps from "../../lib/errors";
 import FetchRequest from "../../lib/fetch-request";
 import { isJsonFormat } from "../../lib/query-utils";
@@ -24,6 +28,8 @@ import { isJsonFormat } from "../../lib/query-utils";
 export default function Modification({
   modification,
   gene,
+  sources,
+  documents,
   isJson,
   attribution = null,
 }) {
@@ -40,6 +46,8 @@ export default function Modification({
               <DataItemValue>{modification.modality}</DataItemValue>
               <DataItemLabel>Cas</DataItemLabel>
               <DataItemValue>{modification.cas}</DataItemValue>
+              <DataItemLabel>Cas Species</DataItemLabel>
+              <DataItemValue>{modification.cas_species}</DataItemValue>
               {modification.fused_domain && (
                 <>
                   <DataItemLabel>Fused Domain</DataItemLabel>
@@ -56,6 +64,12 @@ export default function Modification({
                   </DataItemValue>
                 </>
               )}
+              {modification.description && (
+                <>
+                  <DataItemLabel>Description</DataItemLabel>
+                  <DataItemValue>{modification.description}</DataItemValue>
+                </>
+              )}
               {modification.product_id && (
                 <>
                   <DataItemLabel>Product ID</DataItemLabel>
@@ -70,8 +84,28 @@ export default function Modification({
                   </DataItemValue>
                 </>
               )}
+              {(modification.lot_id ||
+                modification.product_id ||
+                sources.length > 0) && (
+                <>
+                  <DataItemLabel>Sources</DataItemLabel>
+                  <DataItemValue>
+                    <ProductInfo
+                      lotId={modification.lot_id}
+                      productId={modification.product_id}
+                      sources={sources}
+                    />
+                  </DataItemValue>
+                </>
+              )}
             </DataArea>
           </DataPanel>
+          {documents.length > 0 && (
+            <>
+              <DataAreaTitle>Documents</DataAreaTitle>
+              <DocumentTable documents={documents} />
+            </>
+          )}
           <Attribution attribution={attribution} />
         </JsonDisplay>
       </EditableItem>
@@ -82,10 +116,14 @@ export default function Modification({
 Modification.propTypes = {
   // Modification to display
   modification: PropTypes.object.isRequired,
+  // Documents treatment
+  documents: PropTypes.arrayOf(PropTypes.object).isRequired,
   // The gene referenced by the Modification
   gene: PropTypes.object,
   // Attribution for the modification
   attribution: PropTypes.object,
+  // Source lab or source for this sample
+  sources: PropTypes.arrayOf(PropTypes.object),
   // Is the format JSON?
   isJson: PropTypes.bool.isRequired,
 };
@@ -107,6 +145,15 @@ export async function getServerSideProps({ params, req, query }) {
       req.headers.cookie
     );
     const gene = await request.getObject(modification.tagged_protein, null);
+    let sources = [];
+    if (modification.sources?.length > 0) {
+      sources = await request.getMultipleObjects(modification.sources, null, {
+        filterErrors: true,
+      });
+    }
+    const documents = modification.documents
+      ? await requestDocuments(modification.documents, request)
+      : [];
     return {
       props: {
         modification,
@@ -114,8 +161,10 @@ export async function getServerSideProps({ params, req, query }) {
           title: modification.summary,
         },
         breadcrumbs,
+        documents,
         gene,
         attribution,
+        sources,
         isJson,
       },
     };
