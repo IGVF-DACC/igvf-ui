c6f8ee38957d5521540107882e64857570c136ad
khine <khzlin@stanford.edu>
2023-10-01 23:57:07 -0700

    IGVF-1039-audits-page

---
+++ pages/audits.js
@@ -0,0 +1,65 @@
+// node_modules
+import PropTypes from "prop-types";
+import _ from "lodash";
+// component
+import { DataItemValue } from "../components/data-area";
+import Markdown from "../components/Markdown";
+// lib
+import errorObjectToProps from "../lib/errors";
+import FetchRequest from "../lib/fetch-request";
+
+export default function AuditDoc({ uniqueDetails }) {
+  return (
+    <>
+      <h1 className="tight my-4 text-center text-xl font-bold text-black dark:text-gray-300 sm:text-2xl md:my-10 md:text-3xl lg:text-4xl">
+        Audits
+      </h1>
+      <Markdown
+        markdown={
+          "The IGVF Data and Administration Coordinating Center has established an audit system, utilizing flags, to detect discrepancies in the data. While dependencies ensure metadata accuracy within an individual object, audits primarily focus on validating metadata between linked objects. For example, a specific audit has been implemented to verify the linkage of Biosamples with donors sharing the same taxa. Within each audit category, there could be one or more icons, each assigned a distinct color corresponding to the severity level of the audit category."
+        }
+      />
+      <DataItemValue>{uniqueDetails.join(", ")}</DataItemValue>
+    </>
+  );
+}
+
+AuditDoc.propTypes = {
+  uniqueDetails: PropTypes.object,
+};
+
+export async function getServerSideProps({ req }) {
+  const request = new FetchRequest({ cookie: req.headers.cookie });
+  const auditDoc = await request.getObject("/static/doc/auditdoc.json");
+  if (FetchRequest.isResponseSuccess(auditDoc)) {
+    const objectTypes = Object.keys(auditDoc);
+    const details = Object.keys(auditDoc).map((auditCode) => {
+      const auditObject = auditCode.split(".")[2];
+      return auditObject;
+      // Get each audit object within the auditDocObject.
+      // const audit = auditDoc[auditCode];
+      // audit = { audit_category: "missing stuff", audit_detail: "Missing Stuff", audit_levels: ["WARNING"]}
+      // return audit.audit_detail;
+      // return "Missing Stuff", then "Missing Other Stuff", and then "Missing More Stuff"
+    });
+    const uniqueDetails = [...new Set(details)];
+    console.log(uniqueDetails);
+    return {
+      props: {
+        auditDoc,
+        objectTypes,
+        uniqueDetails,
+      },
+    };
+  }
+  return errorObjectToProps(auditDoc);
+}
+
+function makeArray({ auditDoc }) {
+  Object.keys(auditDoc).map((key) => {
+    return {
+      key: key.split(".")[2],
+      ...auditDoc[key],
+    };
+  });
+}
