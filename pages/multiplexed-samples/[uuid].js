2b106e69adcb01cc229bad7a77ff5c566a394457
Eric Douglass <ericdlrv@stanford.edu>
2023-10-16 14:26:11 -0700

    Adding data item listing all the construct library sets in a given multiplexed sample

--- pages/multiplexed-samples/[uuid].js
+++ pages/multiplexed-samples/[uuid].js
@@ -33,6 +33,7 @@ import {
 import errorObjectToProps from "../../lib/errors";
 import FetchRequest from "../../lib/fetch-request";
 import { isJsonFormat } from "../../lib/query-utils";
+import { Link } from 'next/link';
 
 export default function MultiplexedSample({
   multiplexedSample,
@@ -62,6 +63,13 @@ export default function MultiplexedSample({
                     <DataItemValue>
                       {multiplexedSample.cellular_sub_pool}
                     </DataItemValue>
+                  </>
+                )}
+                {/* Property will exist since it's a calculated property */}
+                {multiplexedSample.construct_library_sets.length > 0 && (
+                  <>
+                    <DataItemLabel>Construct Library Sets</DataItemLabel>
+                    <DataItemValue>{multiplexedSample.construct_library_sets.map((lib) => <Link key={lib.accesstion} href={`${lib["@id"]}`}>{lib.accession}</Link>).join(", ")}</DataItemValue>
                   </>
                 )}
               </SampleDataItems>
