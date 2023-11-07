d16e787ccdd74a8420abf1e782d087759d89a3d6
Forrest Tanaka <fytanaka@stanford.edu>
2023-11-07 13:06:21 -0800

    IGVF-1245 Make Cypress page tests idempotent (#354)
    * Update npm packages.
    * Convert page component tests to be idempotent. Clean up ESLint errors in edit.cy.js test from chaining.
    * Add Cypress tests that I’d forgotten for the CHEVRON_NAV component.
    * Allow Cypress page tests to run more than once without restarting igvfd.
    * Instead of modifying an existing page in the Cypress page tests, create a new page and then modify it.

--- cypress/e2e/page-component.cy.js
+++ cypress/e2e/page-component.cy.js
@@ -19,8 +19,9 @@ describe("page-component tests", () => {
     );
 
     // Fill in the metadata and save the page.
-    cy.get("#name").type("image-aligned-page");
-    cy.get("#title").type("Image Aligned Page");
+    const now = new Date().getTime();
+    cy.get("#name").type(`image-aligned-page-${now}`);
+    cy.get("#title").type(`Image Aligned Page ${now}`);
     cy.get(`[aria-label="Save edits to page"]`).click();
 
     // Make sure the image is displayed.
@@ -48,12 +49,13 @@ describe("page-component tests", () => {
     );
 
     // Fill in the metadata and save the page.
-    cy.get("#name").type("page-navigation-page");
-    cy.get("#title").type("Page Navigation Page");
+    const now = new Date().getTime();
+    cy.get("#name").type(`page-navigation-page-${now}`);
+    cy.get("#title").type(`Page Navigation Page ${now}`);
     cy.get(`[aria-label="Save edits to page"]`).click();
 
     // Make sure the navigation bar displays.
-    cy.contains("Page Navigation Page");
+    cy.contains(`Page Navigation Page ${now}`);
     cy.get("[data-testid='page-blocks'] nav a")
       .eq(0)
       .should("have.attr", "href", "#item1")
@@ -86,8 +88,9 @@ describe("page-component tests", () => {
     );
 
     // Fill in the metadata and save the page.
-    cy.get("#name").type("youtube-video-page");
-    cy.get("#title").type("YouTube Video Page");
+    const now = new Date().getTime();
+    cy.get("#name").type(`youtube-video-page-${now}`);
+    cy.get("#title").type(`YouTube Video Page ${now}`);
     cy.get(`[aria-label="Save edits to page"]`).click();
 
     // Make sure the YouTube video displays as an embedded iframe.
@@ -97,5 +100,62 @@ describe("page-component tests", () => {
       "src",
       "https://www.youtube.com/embed/Q8TXgCzxEnw?start=200"
     );
+  });
+
+  it("displays the CHEVRON_NAV component", () => {
+    cy.loginAuth0(Cypress.env("AUTH_USERNAME"), Cypress.env("AUTH_PASSWORD"));
+    cy.contains("Cypress Testing");
+    cy.wait(1000);
+
+    // Add a new page.
+    cy.visit("/");
+    cy.get("[data-testid=navigation-data-model]").click();
+    cy.get("[data-testid=navigation-schemas]").click();
+    cy.get(`[aria-label="Add Page"]`).click();
+
+    // Add an image aligned component.
+    cy.get("#block1-type").select("Component");
+    cy.get("#block1").type("CHEVRON_NAV{enter}");
+    cy.get("#block1").type("First Topic=#first-topic|404080{enter}");
+    cy.get("#block1").type(
+      "Second Topic=https://www.genome.gov/|72c6c2{enter}"
+    );
+    cy.get("#block1").type("Third Topic=/tissues/IGVFSM0000DDDD|c0c0c0");
+
+    // Fill in the metadata and save the page.
+    const now = new Date().getTime();
+    cy.get("#name").type(`chevron-nav-page-${now}`);
+    cy.get("#title").type(`Chevron Navigation Page ${now}`);
+    cy.get(`[aria-label="Save edits to page"]`).click();
+
+    // Make sure the chevron navigation component appears and has the correct links.
+    cy.contains(`Chevron Navigation Page ${now}`);
+    cy.get(`[data-testid="chevron-navigation"] ul li a`)
+      .eq(0)
+      .should("have.attr", "href", "#first-topic")
+      .should(
+        "have.attr",
+        "style",
+        "color: rgb(255, 255, 255); background-color: rgb(64, 64, 128);"
+      )
+      .and("have.text", "First Topic");
+    cy.get(`[data-testid="chevron-navigation"] ul li a`)
+      .eq(1)
+      .should("have.attr", "href", "https://www.genome.gov/")
+      .should(
+        "have.attr",
+        "style",
+        "color: rgb(255, 255, 255); background-color: rgb(114, 198, 194);"
+      )
+      .and("have.text", "Second Topic");
+    cy.get(`[data-testid="chevron-navigation"] ul li a`)
+      .eq(2)
+      .should("have.attr", "href", "/tissues/IGVFSM0000DDDD")
+      .should(
+        "have.attr",
+        "style",
+        "color: rgb(0, 0, 0); background-color: rgb(192, 192, 192);"
+      )
+      .and("have.text", "Third Topic");
   });
 });
