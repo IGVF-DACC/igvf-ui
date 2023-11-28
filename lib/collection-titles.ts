469517b44cfad420dc3def3bf6e3a08edd6c782b
Eric Douglass <ericdlrv@stanford.edu>
2023-11-27 17:41:22 -0800

    converting collection-titles and constants to typescript

---
+++ lib/collection-titles.ts
@@ -0,0 +1,15 @@
+// libs
+import { DataProviderObject } from "../globals";
+import FetchRequest from "./fetch-request";
+
+/**
+ * Loads the mapping of schema name, @type, and collection names to human-readable titles.
+ * @param {string} session Authentication session object
+ * @returns {Promise<DataProviderObject | null>} Promise that resolves to the /collection-titles/ object
+ */
+export default async function getCollectionTitles(dataProviderUrl: string): Promise<DataProviderObject | null> {
+  const request = new FetchRequest();
+  return (
+    await request.getObjectByUrl(`${dataProviderUrl}/collection-titles/`)
+  ).optional();
+}
