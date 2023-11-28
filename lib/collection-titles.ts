249768accd0d2f132e6c7a3d6218d2c6c7174a93
Eric Douglass <ericdlrv@stanford.edu>
2023-11-27 17:44:47 -0800

    formatting

--- lib/collection-titles.ts
+++ lib/collection-titles.ts
@@ -7,7 +7,9 @@ import FetchRequest from "./fetch-request";
  * @param {string} session Authentication session object
  * @returns {Promise<DataProviderObject | null>} Promise that resolves to the /collection-titles/ object
  */
-export default async function getCollectionTitles(dataProviderUrl: string): Promise<DataProviderObject | null> {
+export default async function getCollectionTitles(
+  dataProviderUrl: string
+): Promise<DataProviderObject | null> {
   const request = new FetchRequest();
   return (
     await request.getObjectByUrl(`${dataProviderUrl}/collection-titles/`)
