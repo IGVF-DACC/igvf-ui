cfeb88a3f40f5ed069cbcb2ce7029c911b812a1a
forresttanaka <fytanaka@stanford.edu>
2023-12-20 13:40:19 -0800

    IGVF-1364-tooltips
    * Update npm packages.
    * Integrate new tooltip code. Convert schema index page to use the new tooltips.
    * Remove react-popper npm package as it was needed to adjust the placement of headless-ui-based tooltips, but with floating-ui-based tooltips having this functionality built in, we don’t need this package anymore.
    * Update the comments.
    * Add TooltipRef and Tooltip Jest tests. Removed checking for invalid React children because I can’t think of a way to even check that.
    * Add documentation for tooltips. Update the comments.
    * Correct the tooltip documentation.
    * Temporarily point UI demo at the igvfd dev demo.

---
+++ components/__tests__/tooltip.test.js
@@ -0,0 +1,41 @@
+import { act, fireEvent, render, screen } from "@testing-library/react";
+import { Tooltip, TooltipRef, useTooltip } from "../tooltip";
+
+describe("Test tooltips", () => {
+  it("should render a tooltip", async () => {
+    jest.useFakeTimers();
+
+    function TestComponent() {
+      const tooltipAttr = useTooltip("test");
+
+      return (
+        <>
+          <TooltipRef tooltipAttr={tooltipAttr}>
+            <button>Tooltip Reference</button>
+          </TooltipRef>
+          <Tooltip tooltipAttr={tooltipAttr}>Tooltip content</Tooltip>
+        </>
+      );
+    }
+
+    render(<TestComponent />);
+
+    const button = screen.getByText("Tooltip Reference");
+    expect(button).toHaveAttribute("aria-describedby", "tooltip-test");
+    expect(screen.queryByRole("tooltip")).not.toBeInTheDocument();
+
+    // Hover over the button and make sure the tooltip appears after a delay.
+    fireEvent.mouseEnter(button);
+    const tooltip = await screen.findByTestId("tooltip-test");
+    expect(tooltip).toHaveTextContent("Tooltip content");
+
+    // Move the mouse away and make sure the tooltip disappears after a delay.
+    fireEvent.mouseLeave(button);
+    act(() => {
+      jest.runAllTimers();
+    });
+    expect(screen.queryByRole("tooltip")).not.toBeInTheDocument();
+
+    jest.useRealTimers();
+  });
+});
--- components/tooltip.js
+++ components/tooltip.js
@@ -1,41 +1,163 @@
+/**
+ * This module lets you have a tooltip that appears when the user hovers over an element. For
+ * documentation, see `./docs/tooltip.md`.
+ */
+
 // node_modules
-import { Popover } from "@headlessui/react";
-import { useState } from "react";
-import { usePopper } from "react-popper";
+import {
+  arrow,
+  autoPlacement,
+  FloatingArrow,
+  offset,
+  shift,
+  useFloating,
+  useFocus,
+  useHover,
+  useInteractions,
+} from "@floating-ui/react";
 import PropTypes from "prop-types";
-import { QuestionMarkCircleIcon } from "@heroicons/react/20/solid";
+import { Children, cloneElement, useRef, useState } from "react";
+// lib
+import { toShishkebabCase } from "../lib/general";
 
 /**
- * When the children element is clicked a panel comes up on
- * top of the page with the given text content.
+ * Height of the tooltip arrow (the small pointer from the tooltip to the tooltip ref) in pixels.
  */
-export default function Tooltip({ content, children }) {
-  const [referenceElement, setReferenceElement] = useState();
-  const [popperElement, setPopperElement] = useState();
-  const { styles, attributes } = usePopper(referenceElement, popperElement);
+const ARROW_HEIGHT = 10;
 
-  return (
-    <Popover className="relative">
-      <Popover.Button
-        as="div"
-        ref={setReferenceElement}
-        className="cursor-pointer"
+/**
+ * Width of the tooltip arrow in pixels is the height * 1.4, rounded to the nearest whole number.
+ */
+const ARROW_WIDTH = Math.round(ARROW_HEIGHT * 1.4);
+
+/**
+ * Milliseconds to delay before showing or hiding the tooltip.
+ */
+const TOOLTIP_DELAY = 500;
+
+/**
+ * Call this custom hook within any component that contains a tooltip. It returns props that you
+ * need to pass to both `<TooltipRef>` and `<Tooltip>`. It can only control one tooltip, so if you
+ * need to have more than one, you need to call this hook once for each tooltip.
+ *
+ * You generally assign the return results to a variable named `tooltipAttr`, and then pass that
+ * variable to both `<TooltipRef>` and `<Tooltip>` in their `tooltipAttr` properties.
+ *
+ * Each tooltip needs an ID unique within the page, which you pass to this hook. It gets used for
+ * accessibility.
+ *
+ * These tooltips use the `floating-ui` npm package. See their documentation at:
+ * https://floating-ui.com/docs/react
+ *
+ * @param {string} id Unique ID for the tooltip
+ * @returns {object} Object with props for `<TooltipRef>` and `<Tooltip>`
+ */
+export function useTooltip(id) {
+  const [isVisible, setIsVisible] = useState(false);
+  const arrowRef = useRef(null);
+
+  const { context, floatingStyles, refs } = useFloating({
+    middleware: [
+      autoPlacement(),
+      shift(),
+      arrow({ element: arrowRef }),
+      offset(ARROW_HEIGHT),
+    ],
+    onOpenChange: setIsVisible,
+    open: isVisible,
+  });
+
+  const hover = useHover(context, {
+    delay: TOOLTIP_DELAY,
+  });
+  const focus = useFocus(context);
+  const { getReferenceProps, getFloatingProps } = useInteractions([
+    hover,
+    focus,
+  ]);
+
+  // Return the tooltip attributes that ties together a <TooltipRef>/<Tooltip> pair.
+  return {
+    id: `tooltip-${toShishkebabCase(id)}`,
+    refEl: refs.setReference,
+    refProps: getReferenceProps,
+    tooltipEl: refs.setFloating,
+    tooltipProps: getFloatingProps,
+    context,
+    arrowRef,
+    styles: floatingStyles,
+    isVisible,
+  };
+}
+
+/**
+ * Wrap the element that you want to trigger the tooltip with this component. You can only wrap one
+ * element; more than one causes an error. This tooltip ref wrapper can contain most any kind of
+ * HTML element, though a focusable one like a button or link is recommended for accessibility,
+ * even if it does nothing when the user clicks it.
+ */
+export function TooltipRef({ tooltipAttr, children }) {
+  // Make sure only one child exists. Add the floating-ui refs and its other props to this child.
+  const child = Children.only(children);
+  const clonedElement = cloneElement(child, {
+    ref: tooltipAttr.refEl,
+    "aria-describedby": tooltipAttr.id,
+    ...tooltipAttr.refProps(),
+  });
+
+  return <>{clonedElement}</>;
+}
+
+TooltipRef.propTypes = {
+  // Object returned by `useTooltip()`
+  tooltipAttr: PropTypes.shape({
+    id: PropTypes.string,
+    refEl: PropTypes.func,
+    refProps: PropTypes.func,
+  }),
+};
+
+/**
+ * Wrap the content that you want to show in the tooltip with this component. It can contain one or
+ * many elements. Make sure your content works with both light and dark mode, and with different
+ * browser widths.
+ */
+export function Tooltip({ tooltipAttr, children }) {
+  if (tooltipAttr.isVisible) {
+    return (
+      <div
+        className="max-w-[90%] rounded-md border border-gray-500 bg-gray-800 px-2 py-1 text-xs text-white drop-shadow-md md:max-w-[40%] 2xl:max-w-[20%] dark:border-white dark:bg-gray-300 dark:text-black"
+        ref={tooltipAttr.tooltipEl}
+        style={tooltipAttr.styles}
+        role="tooltip"
+        {...tooltipAttr.tooltipProps()}
+        id={tooltipAttr.id}
+        data-testid={tooltipAttr.id}
       >
-        {children ? children : <QuestionMarkCircleIcon className="h-4 w-4" />}
-      </Popover.Button>
-      <Popover.Overlay className="fixed inset-0 bg-black opacity-30" />
-      <Popover.Panel
-        ref={setPopperElement}
-        style={styles.popper}
-        {...attributes.popper}
-        className="bg-panel z-10 absolute left-1/2 mt-3 w-screen max-w-sm -translate-x-1/2 transform px-4 sm:px-0 lg:max-w-3xl rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden border border-button-secondary prose dark:prose-invert"
-      >
-        <div className="text-left ml-2">{content}</div>
-      </Popover.Panel>
-    </Popover>
-  );
+        {children}
+        <FloatingArrow
+          className="fill-gray-800 dark:fill-gray-300 [&>path:first-of-type]:stroke-gray-500 dark:[&>path:first-of-type]:stroke-white [&>path:last-of-type]:stroke-gray-800 dark:[&>path:last-of-type]:stroke-gray-300"
+          ref={tooltipAttr.arrowRef}
+          context={tooltipAttr.context}
+          height={ARROW_HEIGHT}
+          width={ARROW_WIDTH}
+          strokeWidth={1}
+        />
+      </div>
+    );
+  }
+  return null;
 }
 
 Tooltip.propTypes = {
-  content: PropTypes.string,
+  // Object returned by `useTooltip()`
+  tooltipAttr: PropTypes.shape({
+    id: PropTypes.string,
+    tooltipEl: PropTypes.func,
+    tooltipProps: PropTypes.func,
+    context: PropTypes.object,
+    styles: PropTypes.object,
+    arrowRef: PropTypes.object,
+    isVisible: PropTypes.bool,
+  }),
 };
