313c89319765dc3c2c7d58bc1e3e207d5ee74279
forresttanaka <fytanaka@stanford.edu>
2024-07-02 15:20:47 -0700

    IGVF-1521-table-report-links
    Remove the report link from the Modifications table until Modification becomes an abstract class, and CrisprModification and DegronModification become the real object types. Keep `reportLink` and `reportLabel` in the calls to the modification table because they’ll need to return anyway.

--- components/modification-table.js
+++ components/modification-table.js
@@ -1,8 +1,7 @@
 // node_modules
-import { TableCellsIcon } from "@heroicons/react/20/solid";
 import PropTypes from "prop-types";
 // components
-import { DataAreaTitle, DataAreaTitleLink } from "./data-area";
+import { DataAreaTitle } from "./data-area";
 import LinkedIdAndStatus from "./linked-id-and-status";
 import SortableGrid from "./sortable-grid";
 
@@ -25,20 +24,11 @@ const modificationsColumns = [
  */
 export default function ModificationTable({
   modifications,
-  reportLink = null,
-  reportLabel = null,
   title = "Modifications",
 }) {
   return (
     <>
-      <DataAreaTitle>
-        {title}
-        {reportLink && reportLabel && (
-          <DataAreaTitleLink href={reportLink} label={reportLabel}>
-            <TableCellsIcon className="h-4 w-4" />
-          </DataAreaTitleLink>
-        )}
-      </DataAreaTitle>
+      <DataAreaTitle>{title}</DataAreaTitle>
       <SortableGrid
         data={modifications}
         columns={modificationsColumns}
@@ -51,10 +41,6 @@ export default function ModificationTable({
 ModificationTable.propTypes = {
   // Modifications to display
   modifications: PropTypes.arrayOf(PropTypes.object).isRequired,
-  // Link to the report; use with `reportLabel`
-  reportLink: PropTypes.string,
-  // Label for the report link; use with `reportLink`
-  reportLabel: PropTypes.string,
   // Title for the table if not "Modifications"
   title: PropTypes.string,
 };
