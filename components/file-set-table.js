c6bda0b82f4949223b143b2925b816311883f82d
Forrest Tanaka <fytanaka@stanford.edu>
2024-07-24 21:26:13 -0700

    IGVF-1822 Display a Files column in the Auxiliary Datasets table on MeasurementSet pages (#561)
    * Update npm packages.
    * Modify the file-set table to display any files embedded in the given file sets.
    * Remove a now-unused new component. Correct a comment.
    * Update the hide function for the Files column of the file-set table to hide the Files column even if the client requested one if no files to display. Correct some comments.
    * Make the Files column of the FileSet table non-sortable.

--- components/file-set-table.js
+++ components/file-set-table.js
@@ -1,9 +1,11 @@
 // node_modules
 import { TableCellsIcon } from "@heroicons/react/20/solid";
+import Link from "next/link";
 import PropTypes from "prop-types";
 // components
 import { DataAreaTitle, DataAreaTitleLink } from "./data-area";
 import LinkedIdAndStatus from "./linked-id-and-status";
+import SeparatedList from "./separated-list";
 import SortableGrid from "./sortable-grid";
 import { AliasesCell } from "./table-cells";
 // lib
@@ -23,6 +25,42 @@ const fileSetColumns = [
     sorter: (item) => item.summary.toLowerCase(),
   },
   {
+    id: "files",
+    title: "Files",
+    display: ({ source, meta }) => {
+      if (source.files.length > 0) {
+        const files = meta.fileSetMeta?.fileFilter
+          ? meta.fileSetMeta.fileFilter(source.files)
+          : source.files;
+        return (
+          <SeparatedList>
+            {files.map((file) => (
+              <Link key={file["@id"]} href={file["@id"]}>
+                {file.accession}
+              </Link>
+            ))}
+          </SeparatedList>
+        );
+      }
+    },
+    hide: (data, columns, meta) => {
+      if (meta.fileSetMeta?.showFileSetFiles) {
+        // Hide the column if the client didn't request it, or if they did but there are no files
+        // to display after filtering, if the client provided a filter function.
+        const allFiles = data.reduce(
+          (acc, fileSet) => acc.concat(fileSet.files),
+          []
+        );
+        const filteredFiles = meta.fileSetMeta?.fileFilter
+          ? meta.fileSetMeta.fileFilter(allFiles)
+          : allFiles;
+        return filteredFiles.length === 0;
+      }
+      return true;
+    },
+    isSortable: false,
+  },
+  {
     id: "aliases",
     title: "Aliases",
     display: ({ source }) => <AliasesCell source={source} />,
@@ -37,35 +75,39 @@ const fileSetColumns = [
 
 /**
  * Display a sortable table of the given file sets. Optionally display a link to a report page of
- * the file sets in this table.
- *
- * To allow this report link to work, the file sets in this table must each include a link back to
- * the currently displayed item. You specify the nature of this link through the `identifierProp`
- * and `itemIdentifier` properties of `reportLinkInfo`. For example, if the file sets in this table
- * include a `samples` property containing the accession of the currently displayed item, then
- * `identifierProp` would contain `samples.accession` and `itemIdentifier` would contain the
- * accession of the currently displayed item.
+ * the file sets in this table. To allow this report link to work, the file sets in this table
+ * must each include a link back to the currently displayed item.
  *
  * If you want a link to the report page, you must provide either the `reportLink` or
  * `reportLinkSpecs` property (not both). If you provide `reportLink`, it must contain the
- * complete URL of the report page. If you provide `reportLinkSpecs`, it must contain the
+ * complete path of the report page. If you provide `reportLinkSpecs`, it must contain the
  * properties needed to construct the URL of the report page. The `reportLinkSpecs` object must
  * contain the following properties:
  *
- * Summary of the `reportLinkInfo` properties:
- *
- * - reportType: `@type` of the file sets referring to the currently displayed item. The report page
+ * - fileSetType: `@type` of the file sets referring to the currently displayed item. The report page
  *   displays only objects of this type. You can use the `FileSet` abstract type if needed here.
  *
  * - identifierProp: Property of the report items that links back to the currently displayed item
  *
  * - itemIdentifier: ID of the currently displayed item that the report items link back to.
+ *
+ * You can optionally display a Files column in the table. Each cell of this column shows linked
+ * accessions of files belonging to the file set displayed in that row. To show this column,
+ * include the `fileSetMeta` property with the following properties:
+ *
+ * - showFileSetFiles: True to show the Files column
+ *
+ * - fileFilter: Optional function to filter the files to display in the Files column. This
+ *   function takes an array of file objects displayed in the cell of the File column and returns
+ *   an array of file objects to include in the cell. If you don't provide this function, the cell
+ *   displays all files belonging to the file set in the row.
  */
 export default function FileSetTable({
   fileSets,
   reportLink = "",
   reportLinkSpecs = null,
   title = "File Sets",
+  fileSetMeta = null,
 }) {
   // Generate the link to the report page if requested.
   const composedReportLink = reportLinkSpecs
@@ -93,6 +135,7 @@ export default function FileSetTable({
         data={fileSets}
         columns={fileSetColumns}
         keyProp="@id"
+        meta={{ fileSetMeta }}
         pager={{}}
       />
     </>
@@ -115,4 +158,11 @@ FileSetTable.propTypes = {
   }),
   // Title of the table if not "File Sets"
   title: PropTypes.string,
+  // Metadata for display options
+  fileSetMeta: PropTypes.exact({
+    // True to show a column for the files belonging to each file set
+    showFileSetFiles: PropTypes.bool,
+    // Function to filter the files to display
+    fileFilter: PropTypes.func,
+  }),
 };
