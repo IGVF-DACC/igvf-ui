3da58f6b3a483d79694fd5bfc24893f9b6871f67
Forrest Tanaka <fytanaka@stanford.edu>
2024-07-27 09:56:01 -0700

    IGVF-1837 Filter embeddable file-set chart with query string (#564)
    * Handle a `month=yyyy-MM` query-string on `/content/fileset-chart/?month=2024-03.
    * Update default property values for consistency.

--- pages/content/fileset-chart.js
+++ pages/content/fileset-chart.js
@@ -13,13 +13,14 @@ import { requestDatasetSummary } from "../../lib/common-requests";
 import { errorObjectToProps } from "../../lib/errors";
 import FetchRequest from "../../lib/fetch-request";
 
-export default function FileSetChart({ fileSets }) {
+export default function FileSetChart({ fileSets, staticSelectedMonth = "" }) {
   return (
     <>
       {fileSets.length > 0 && (
         <ChartFileSetLab
           fileSets={fileSets}
           title="Data Sets Produced by IGVF Labs"
+          staticSelectedMonth={staticSelectedMonth}
         />
       )}
     </>
@@ -29,15 +30,18 @@ export default function FileSetChart({ fileSets }) {
 FileSetChart.propTypes = {
   // File sets to display in the chart
   fileSets: PropTypes.arrayOf(PropTypes.object).isRequired,
+  // The month to display in the chart as specified in the query string
+  staticSelectedMonth: PropTypes.string,
 };
 
-export async function getServerSideProps({ req }) {
+export async function getServerSideProps({ req, query }) {
   const request = new FetchRequest({ cookie: req.headers.cookie });
   const results = await requestDatasetSummary(request);
   if (FetchRequest.isResponseSuccess(results)) {
     return {
       props: {
         fileSets: results["@graph"] || [],
+        staticSelectedMonth: query.month || "",
         isStandalonePage: true,
       },
     };
