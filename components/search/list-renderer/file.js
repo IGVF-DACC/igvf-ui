da9989b7854960f1d898b1fbc880087cf6fb1655
forresttanaka <fytanaka@stanford.edu>
2024-09-18 11:00:42 -0700

    IGVF-1947-file-set-embed-crash
    Have the file list-view renderer check if `file_set` is an object or not, and to not render the file set if it’s not an object.

--- components/search/list-renderer/file.js
+++ components/search/list-renderer/file.js
@@ -28,11 +28,20 @@ export default function File({ item: file, accessoryData = null }) {
     file.illumina_read_type,
   ].filter(Boolean);
 
+  // During indexing, `file_set` can contain a path instead of the expected object.
+  const isFileSetEmbedded = typeof file.file_set === "object";
+
   // Get the seqspec_of objects from the accessory data.
   let seqspecOfs = file.seqspec_of
     ? file.seqspec_of.map((seqspecOfFile) => accessoryData?.[seqspecOfFile])
     : [];
   seqspecOfs = seqspecOfs.filter(Boolean);
+
+  const isSupplementVisible =
+    isFileSetEmbedded ||
+    file.content_summary ||
+    seqspecOfs.length > 0 ||
+    file.alternate_accessions?.length > 0;
 
   return (
     <SearchListItemContent>
@@ -45,45 +54,51 @@ export default function File({ item: file, accessoryData = null }) {
         <SearchListItemMeta>
           <span key="lab">{file.lab.title}</span>
         </SearchListItemMeta>
-        <SearchListItemSupplement>
-          <SearchListItemSupplementAlternateAccessions item={file} />
-          {file.content_summary && (
-            <SearchListItemSupplementSection>
-              <SearchListItemSupplementLabel>
-                Content Summary
-              </SearchListItemSupplementLabel>
-              <SearchListItemSupplementContent>
-                {file.content_summary}
-              </SearchListItemSupplementContent>
-            </SearchListItemSupplementSection>
-          )}
-          <SearchListItemSupplementSection>
-            <SearchListItemSupplementLabel>
-              File Set
-            </SearchListItemSupplementLabel>
-            <SearchListItemSupplementContent>
-              <Link href={file.file_set["@id"]}>{file.file_set.summary}</Link>
-            </SearchListItemSupplementContent>
-          </SearchListItemSupplementSection>
-          {seqspecOfs.length > 0 && (
-            <SearchListItemSupplementSection>
-              <SearchListItemSupplementLabel>
-                Seqspec Of
-              </SearchListItemSupplementLabel>
-              <SearchListItemSupplementContent>
-                <SeparatedList isCollapsible>
-                  {seqspecOfs.map((seqspecOf) => {
-                    return (
-                      <Link href={seqspecOf["@id"]} key={seqspecOf["@id"]}>
-                        {seqspecOf.accession}
-                      </Link>
-                    );
-                  })}
-                </SeparatedList>
-              </SearchListItemSupplementContent>
-            </SearchListItemSupplementSection>
-          )}
-        </SearchListItemSupplement>
+        {isSupplementVisible && (
+          <SearchListItemSupplement>
+            <SearchListItemSupplementAlternateAccessions item={file} />
+            {file.content_summary && (
+              <SearchListItemSupplementSection>
+                <SearchListItemSupplementLabel>
+                  Content Summary
+                </SearchListItemSupplementLabel>
+                <SearchListItemSupplementContent>
+                  {file.content_summary}
+                </SearchListItemSupplementContent>
+              </SearchListItemSupplementSection>
+            )}
+            {isFileSetEmbedded && (
+              <SearchListItemSupplementSection>
+                <SearchListItemSupplementLabel>
+                  File Set
+                </SearchListItemSupplementLabel>
+                <SearchListItemSupplementContent>
+                  <Link href={file.file_set["@id"]}>
+                    {file.file_set.summary}
+                  </Link>
+                </SearchListItemSupplementContent>
+              </SearchListItemSupplementSection>
+            )}
+            {seqspecOfs.length > 0 && (
+              <SearchListItemSupplementSection>
+                <SearchListItemSupplementLabel>
+                  Seqspec Of
+                </SearchListItemSupplementLabel>
+                <SearchListItemSupplementContent>
+                  <SeparatedList isCollapsible>
+                    {seqspecOfs.map((seqspecOf) => {
+                      return (
+                        <Link href={seqspecOf["@id"]} key={seqspecOf["@id"]}>
+                          {seqspecOf.accession}
+                        </Link>
+                      );
+                    })}
+                  </SeparatedList>
+                </SearchListItemSupplementContent>
+              </SearchListItemSupplementSection>
+            )}
+          </SearchListItemSupplement>
+        )}
         <SearchListItemQuality item={file} />
       </SearchListItemMain>
     </SearchListItemContent>
