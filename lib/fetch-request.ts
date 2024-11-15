01a53145e8cfa552efcd58c9430da65d248503eb
forresttanaka <fytanaka@stanford.edu>
2024-11-15 11:10:20 -0800

    IGVF-2041-text-file-preview
    Add debugging code to `getZippedPreviewText()` to see how very large files get handled.

--- lib/fetch-request.ts
+++ lib/fetch-request.ts
@@ -575,6 +575,12 @@ export default class FetchRequest {
     let lineCount = 0;
     while (!done && lineCount < MAX_READ_LINES) {
       const { value, done: readerDone } = await reader.read();
+      console.log(
+        "************** READ:%s-%s--%s",
+        lineCount,
+        value?.length || "NO READ",
+        readerDone
+      );
       if (value) {
         // Unzip the chunk of text into the pako buffer and add it to our text accumulator. In some
         // cases pako can return `undefined`, so ignore those.
@@ -582,6 +588,10 @@ export default class FetchRequest {
         if (inflator.err) {
           done = true;
         } else if (inflator.result) {
+          console.log(
+            "************** INFLATOR:%s",
+            inflator.result?.length || "NO INFLATOR"
+          );
           decompressedText += inflator.result;
           if (decompressedText) {
             // If lineCount exceeds maxLines, stop after the `maxLines` line.
