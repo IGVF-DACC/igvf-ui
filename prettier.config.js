cdcbe73348743e6c087b9426c8238f7909265104
Forrest Tanaka <fytanaka@stanford.edu>
2025-01-21 15:49:47 -0800

    IGVF-2143 Remove facet groups and display all facets (#689)
    * Update npm packages.
    * Update floating-ui npm package.
    * Update marked-react npm package.
    * Remove facet groups. Display all available facets.
    * Remove last vestiges of facet groups.
    * Add facet collapsing mechanism.
    * Have the audit facet titles allow collapsing and expanding.
    * Remove facet All and None buttons.
    * Use light-blue hover and indicator that the facet is open. Reformat facets so the borders go to the edge of the panel.
    * Try to fix the prettier conflicts.
    * Try using .prettierrc.js instead of a JSON file.
    * Revert prettier changes.
    * Animate the opening and closing of facets.
    * Animate the expanding of a facet with lots of terms. We have a problem now where facets with custom terms might display a blank space with borders. I might need to add a customizable filtering function to the facet mechanism.
    * Filter out internal issues from facets if not logged in.
    * Add facet term counter dashed bar to each facet.
    * Make entire collapsed facet box clickable.
    * Saves and retrieves facet configurations.
    * Use pre-defined constants for the facet-config endpoint.
    * Move much of the function to open and close facets outside the main component to reduce parsing on re-renders.
    * Temporary update to facet colors. Still need to work on it.
    * Change the colors of the opened facet titles, and respond to dark mode.
    * Display segments of counts without blank segments. Don’t take current selections into account until the next commit.
    * Add a final version of the facet term counter.
    * Fixes to Jest tests, but we still don’t have full coverage.
    * Get full coverage for the facet-list Jest test.
    * Add test for facet-term-count.tsx.
    * Get full Jest coverage for standard-terms.js.
    * Get full coverage for lib/facets.ts.
    * Get full Jest test coverage for search-results.ts, and for all Jest tests.
    * Fix linting issues.
    * Pre-commit modifications.
    * Revert to old prettier config, where .prettierrc is for pre-commit and prettier.config.js is for VSCode.
    * Update the facet Cypress test.
    * Set facet store expiration for a week.
    * Display negative facets in the facet term counter.
    * Update Jest tests.
    * Constrain facet term count indicators to one row that fades out on the right side.
    * Remove the now-unused type facet term module.
    * Delete a stray empty file. Update some Tailwind CSS class orders. Replace hard-coded HTTP error codes with symbols. Add Typescript to both prettier configurations.
    * In the facet Cypress test, navigate to the InVitroSystem list view instead of loading it directly so Cypress can find the Sample facet more reliably.
    * Change the click target of a facet term to make the facet Cypress test more reliable.
    * See if delays fix the weird Cypress problem.
    * Correct a comment.
    * Change the positive facet counters from lime to green.

--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      options: {
+        parser: "typescript",
+      },
+    },
+  ],
   trailingComma: "es5",
   plugins: ["prettier-plugin-tailwindcss"],
 };
