46cde134c1695eb5bbe71dcd0091ee913b682512
forresttanaka <fytanaka@stanford.edu>
2025-03-14 10:49:49 -0700

    IGVF-2480-checkfiles-version-adjustment
    * Add new conditions for checkfiles_version display. Add utility function to determine if the checkfiles version should be displayed at all.
    * Add the new conditions to the `<CheckfilesVersion>` component Jest tests. Add new Jest tests for checkfiles_version.ts library file.

--- components/__tests__/checkfiles-version.test.tsx
+++ components/__tests__/checkfiles-version.test.tsx
@@ -105,7 +105,7 @@ describe("Test <CheckfilesVersion />", () => {
     expect(screen.getByText("No version due to exemption")).toBeInTheDocument();
   });
 
-  it("renders a link to the root checkfiles versions with no checkfiles_version", () => {
+  it("renders a link to the root checkfiles versions with no checkfiles_version and validated status", () => {
     const file: FileObject = {
       "@context": "/terms/",
       "@id": "/alignment-files/IGVFFI0000ALGN/",
@@ -140,5 +140,72 @@ describe("Test <CheckfilesVersion />", () => {
     expect(
       screen.getByText("Legacy version of checkfiles")
     ).toBeInTheDocument();
+  });
+
+  it("renders a link to the root checkfiles versions with no checkfiles_version and invalidated status", () => {
+    const file: FileObject = {
+      "@context": "/terms/",
+      "@id": "/alignment-files/IGVFFI0000ALGN/",
+      "@type": ["AlignmentFile", "File", "Item"],
+      accession: "IGVFFI0000ALGN",
+      assay_titles: ["yN2H"],
+      assembly: "GRCh38",
+      award: "/awards/HG012012/",
+      content_summary: "unfiltered alignments",
+      content_type: "alignments",
+      controlled_access: false,
+      creation_timestamp: "2025-02-12T15:48:16.484438+00:00",
+      file_format: "bam",
+      file_set: "/analysis-sets/IGVFDS8063WIFB/",
+      reference_files: ["/reference-files/IGVFFI0001GNRF/"],
+      release_timestamp: "2024-03-06T12:34:56Z",
+      status: "released",
+      submitted_file_name: "/Users/igvf/igvf_files/alignments.bam",
+      summary: "GRCh38 unfiltered alignments",
+      upload_status: "invalidated",
+      uuid: "f5c8a277-e11d-ea86-cc75-bc735eb802c8",
+    };
+
+    render(<CheckfilesVersion file={file} />);
+
+    // Test that it renders a link to the root checkfiles versions.
+    const link = screen.getByRole("link");
+    expect(link).toHaveAttribute(
+      "href",
+      "https://github.com/IGVF-DACC/checkfiles/tags"
+    );
+    expect(
+      screen.getByText("Legacy version of checkfiles")
+    ).toBeInTheDocument();
+  });
+
+  it("renders nothing with no checkfiles_version and no validated, invalidated, or exempted status", () => {
+    const file: FileObject = {
+      "@context": "/terms/",
+      "@id": "/alignment-files/IGVFFI0000ALGN/",
+      "@type": ["AlignmentFile", "File", "Item"],
+      accession: "IGVFFI0000ALGN",
+      assay_titles: ["yN2H"],
+      assembly: "GRCh38",
+      award: "/awards/HG012012/",
+      content_summary: "unfiltered alignments",
+      content_type: "alignments",
+      controlled_access: false,
+      creation_timestamp: "2025-02-12T15:48:16.484438+00:00",
+      file_format: "bam",
+      file_set: "/analysis-sets/IGVFDS8063WIFB/",
+      reference_files: ["/reference-files/IGVFFI0001GNRF/"],
+      release_timestamp: "2024-03-06T12:34:56Z",
+      status: "released",
+      submitted_file_name: "/Users/igvf/igvf_files/alignments.bam",
+      summary: "GRCh38 unfiltered alignments",
+      upload_status: "uploading",
+      uuid: "f5c8a277-e11d-ea86-cc75-bc735eb802c8",
+    };
+
+    render(<CheckfilesVersion file={file} />);
+
+    // Test that it renders nothing.
+    expect(screen.queryByRole("link")).toBeNull();
   });
 });
