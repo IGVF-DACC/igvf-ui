658f70830998eb6e2b5a0032d711c99fa07ff354
forresttanaka <fytanaka@stanford.edu>
2025-10-10 15:08:29 -0700

    IGVF-3062-fileset-input-table
    * Request `input_for` file sets using the newly embedded objects on auxiliary-set and measurement-set pages. Change the report link for the Input For table on model-set pages, which has been broken for a while.
    * Make the loop to request `input_for` files safer in case we get an array of paths during indexing.

--- pages/auxiliary-sets/[id].js
+++ pages/auxiliary-sets/[id].js
@@ -265,8 +265,14 @@ export async function getServerSideProps({ params, req, query }) {
     }
 
     const inputFileSetFor =
-      auxiliarySet.input_for?.length > 0
-        ? await requestFileSets(auxiliarySet.input_for, request)
+      auxiliarySet.input_for?.length > 0 &&
+      auxiliarySet.input_for.every(
+        (item) => typeof item === "object" && item["@id"]
+      )
+        ? await requestFileSets(
+            auxiliarySet.input_for.map((inputFor) => inputFor["@id"]),
+            request
+          )
         : [];
 
     let controlFor = [];
