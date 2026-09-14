64bab679aea7bace512f25d71551ed202b4eadad
forresttanaka <fytanaka@stanford.edu>
2026-09-14 11:19:03 -0700

    IGVF-3702-diff-series-matrix
    Revert CDK config to use default backend_url

--- cdk/infrastructure/config.py
+++ cdk/infrastructure/config.py
@@ -59,7 +59,6 @@ config: Dict[str, Any] = {
                     },
                 ],
             },
-            'backend_url': 'https://igvfd-igvf-3702-diff-series-matrix.demo.igvf.org',
             'frontend': {
                 'cpu': 1024,
                 'memory_limit_mib': 2048,
